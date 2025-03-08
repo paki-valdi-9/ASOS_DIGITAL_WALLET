10b8f45781c211b2a26f7496cb998cdbeafe8dd4
paki-valdi-9 <palkourbanek45@gmail.com>
2025-03-08 10:50:41 +0100

    chore: change of remote origin

--- asos_digital_walltet/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-//     plugins: [
-//       '@atlaskit/design-system',
-//     ],
-//     extends: [
-//       'plugin:@atlaskit/design-system/recommended',
-//     ],
-//   };
--- node_modules/@atlaskit/empty-state/dist/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './empty-state';
-export type { Sizes, Width, RenderImageProps, EmptyStateProps } from './types';
